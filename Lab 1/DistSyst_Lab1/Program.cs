f39ca9c6d5192acfa7ff428b7d2a084fd67967c8
Zak (Lenovo) <zakcatherall@outlook.com>
2020-02-17 19:43:46 +0000

    added lab 1

---
+++ Lab 1/DistSyst_Lab1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Runtime.Remoting;
+using System.Runtime.Remoting.Channels;
+using System.Runtime.Remoting.Channels.Tcp;
+
+namespace DistSyst_Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TcpChannel channel = new TcpChannel(80);
+            ChannelServices.RegisterChannel(channel, false);
+            RemotingConfiguration.RegisterWellKnownServiceType(
+            typeof(Translator),
+             "Translate",
+             WellKnownObjectMode.SingleCall);
+            Console.WriteLine("Press the enter key to exit...");
+            Console.ReadLine();
+        }
+    }
+}
