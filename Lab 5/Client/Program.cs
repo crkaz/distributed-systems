82bdead081c3d4a1cec5d032f6f5e3e6d0caf82a
Zak (Lenovo) <zakcatherall@outlook.com>
2020-03-11 15:17:44 +0000

    Completed lab 5 bar some boring extension tasks

---
+++ Lab 5/Client/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Net.Http;
+namespace ConsoleClient
+{
+    class Program
+    {
+        const int TIMEOUT = 10000; // milliseconds
+        static HttpClient client = new HttpClient();
+        static void Main(string[] args)
+        {
+            client.BaseAddress = new Uri("http://localhost:61905/");
+
+            // Tests
+            TestRequest("GetName", "Zak");
+            TestRequest("GetLocation", "Hull", "location");
+            TestRequest("GetInt", "3");
+            //
+
+            Console.ReadKey();
+        }
+
+        static async void TestRequest(string action, string param = "", string fromQuery = null)
+        {
+            try
+            {
+                Task<string> task;
+                if (fromQuery != null)
+                {
+                    task = GetStringAsync("/api/Translate/" + action + "?" + fromQuery + "=" + param);
+                }
+                else
+                {
+                    task = GetStringAsync("/api/Translate/" + action + "/" + param);
+                }
+                if (await Task.WhenAny(task, Task.Delay(TIMEOUT)) == task)
+                { Console.WriteLine(task.Result); }
+                else
+                { Console.WriteLine("Request Timed Out"); }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.GetBaseException().Message);
+            }
+        }
+
+        static async Task<string> GetStringAsync(string path)
+        {
+            string responsestring = "";
+            HttpResponseMessage response = await client.GetAsync(path);
+            responsestring = await response.Content.ReadAsStringAsync();
+            return responsestring;
+        }
+    }
+}
---
+++ Lab 5/lab5/Controllers/TranslateController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace lab5.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class TranslateController : ControllerBase
+    {
+        // GET: api/Translate
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Translate/GetInt/5
+        [HttpGet("{id:int}", Name = "GetInt")]
+        public string GetInt(int id)
+        {
+            string value = id.ToString();
+            return value;
+        }
+
+        // GET: api/Translate/GetString/Zak
+        [HttpGet("{input}")]
+        public string GetString(string input)
+        {
+            return input + "eth";
+        }
+
+        // GET: api/Translate/GetName/Zak
+        [HttpGet("{name}")]
+        [ActionName("GetName")]
+        public string GetName(string name)
+        {
+            return name;
+        }
+
+
+        // GET: api/Translate/GetLocation?location=Hull
+        [HttpGet]
+        [ActionName("GetLocation")]
+        public string Get([FromQuery]string location)
+        {
+            return "You are at " + location;
+        }
+
+        // GET: api/Translate/GetString/Zak
+        [HttpGet("{input}")]
+        public ActionResult<string> GetActionResult(string input)
+        {
+            if (input == "bad")
+            {
+                return BadRequest();
+            }
+            else if (input == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return Ok();
+            }
+        }
+
+        // POST: api/Translate
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/Translate/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
