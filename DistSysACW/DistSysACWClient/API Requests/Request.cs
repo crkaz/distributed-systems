9211446049ade692b7bce3ea3a3b982194e3b183
Zak (Lenovo) <zakcatherall@outlook.com>
2020-04-13 23:09:29 +0100

    Separated everything into classes but abuses public static

---
+++ DistSysACW/DistSysACWClient/API Requests/Request.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Net.Http;
+using System.Text;
+using Newtonsoft.Json;
+using System.Threading.Tasks;
+
+namespace DistSysACWClient
+{
+    class Request
+    {
+        protected static void GetEndpoint(string endpoint)
+        {
+            try
+            {
+                try
+                {
+                    var worker = Connection.Client.GetStringAsync(endpoint);
+                    var response = worker.GetAwaiter().GetResult();
+                    worker.Wait();
+                    Console.WriteLine(response);
+                }
+                catch (HttpRequestException e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+            catch (Exception e) // Catch anything else unexpected.
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        protected static string GetGetEndpoint(string endpoint)
+        {
+            string response = null;
+
+            Task.Run(async () =>
+            {
+                var worker = await Connection.Client.GetAsync(endpoint);
+                //var response = worker.GetAwaiter();//.GetResult();
+                response = await worker.Content.ReadAsStringAsync();
+
+            }).Wait();
+
+            return response;
+        }
+
+        protected static string GetEndpoint(string endpoint, string successResponse, string errorResponse)
+        {
+            try
+            {
+                var worker = Connection.Client.GetStringAsync(endpoint);
+                var response = worker.GetAwaiter().GetResult();
+                worker.Wait();
+                Console.WriteLine(successResponse);
+                return response;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(errorResponse);
+            }
+            return null;
+        }
+
+        protected static void PostEndpoint(string endpoint, string body, Action<HttpResponseMessage> onResponse, bool jsonObj = false)
+        {
+            try
+            {
+                try
+                {
+                    string json = body;
+                    if (!jsonObj)
+                    {
+                        json = JsonConvert.SerializeObject(body);
+                    }
+
+                    var data = new StringContent(json, Encoding.UTF8, "application/json");
+                    var worker = Connection.Client.PostAsync(endpoint, data);
+                    var response = worker.GetAwaiter().GetResult();
+                    worker.Wait();
+
+                    onResponse.Invoke(response);
+                }
+                catch (HttpRequestException e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+            catch (Exception e) // Catch anything else unexpected.
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        protected static void DeleteEndpoint(string endpoint)
+        {
+            try
+            {
+                try
+                {
+                    var worker = Connection.Client.DeleteAsync(endpoint);
+                    var response = worker.GetAwaiter().GetResult();
+                    worker.Wait();
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        Console.WriteLine("True");
+                    }
+                    else
+                    {
+                        Console.WriteLine("False");
+                    }
+                }
+                catch (HttpRequestException e)
+                {
+                    //Console.WriteLine("False"); // Unsure which is correct.
+                    Console.WriteLine(e.Message);
+                }
+            }
+            catch (Exception e) // Catch anything else unexpected.
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
---
+++ DistSysACW/DistSysACWClient/API Requests/TalkbackRequest.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DistSysACWClient
+{
+    class TalkbackRequest : Request
+    {
+        public static void Hello()
+        {
+            string endpoint = "TalkBack/Hello";
+            GetEndpoint(endpoint);
+        }
+
+        public static void Sort(string args)
+        {
+            string endpoint = "TalkBack/Sort?integers=";
+
+            try
+            {
+                if (args.Length < 3) // Array created with a minimum of 3 chars "[_]".
+                {
+                    throw new Exception();
+                }
+
+                args = args.Substring(1, args.Length - 2); // Remove square brackets.
+                string[] parsed = args.Split(',');
+                int i = 0;
+                foreach (string integer in parsed)
+                {
+                    int.Parse(integer); // Will catch non integer args.
+
+                    if (i++ == 0)
+                    {
+                        endpoint += integer;
+                    }
+                    else
+                    {
+                        endpoint += "&integers=" + integer;
+                    }
+                }
+                GetEndpoint(endpoint);
+            }
+            catch
+            {
+                Console.WriteLine("Invalid arguments.");
+            }
+        }
+    }
+}
